2469212dff7fe24dd04d9475144b0d2a33426e8e
Tryd0g0lik <work80@mail.ru>
2024-10-12 12:52:14 +0700

    [master]: 'src\services\pathname.ts' added

---
+++ src/services/pathname.ts
@@ -0,0 +1,17 @@
+/**
+ * Here us need to get a pathname from the page's url.
+ * When a pathname has text of '/change' or '/signUp', or '/loginIn'
+ * We to receive the true else false.
+ * This function we use for to run \
+ * components a 'ChangePasswordFC', 'GoLoginInFC', 'GetFormRegistrationsFC'
+ * @returns true/false
+ */
+export function CheckPathnameOfUrl(): boolean {
+  const pathname = document.location.pathname;
+  const bool = (pathname.includes('/change')) ? true : (
+    pathname.includes('/signUp') ? true : (
+      pathname.includes('/loginIn') ? true : false
+    )
+  );
+  return bool;
+}
